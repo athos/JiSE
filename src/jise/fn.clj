c581660231db8416c0d3867b63509eaa10414ef8
Shogo Ohta <athos0220@gmail.com>
2019-03-11 21:40:15 +0900

    Tweak type hints for jise/fn macro

--- src/jise/fn.clj
+++ src/jise/fn.clj
@@ -3,11 +3,25 @@
   (:require [clojure.core :as c]
             [jise.core :as jise]))
 
+(c/defn- fixup-type-hints [args]
+  (map (c/fn [arg]
+         (if (= (:tag (meta arg) 'Object) 'Object)
+           arg
+           (vary-meta arg dissoc :tag)))
+       args))
+
 (c/defn- emit-fn-class [fname args body]
-  `^:public
-  (jise/class ~fname [clojure.lang.IFn]
-    ^:public ^Object
-    (jise/defm ~'invoke ~args ~@body)))
+  (let [args' (fixup-type-hints args)]
+    `^:public
+    (jise/class ~fname [clojure.lang.IFn]
+      ^:public ^Object
+      (jise/defm ~'invoke ~(vec args')
+        ~@(if-let [diffs (->> (map vector args args')
+                              (remove (c/fn [[s s']] (identical? s s')))
+                              seq)]
+            `((let* ~(into [] cat diffs)
+                ~@body))
+            body)))))
 
 (defmacro fn [args & body]
   (emit-fn-class (gensym 'f) args body))
