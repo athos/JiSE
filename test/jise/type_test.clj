073e451ec5db70d638c4693a87f00a758077a21d
Shogo Ohta <athos0220@gmail.com>
2019-07-02 23:00:57 +0900

    Add some more tests

--- test/jise/type_test.clj
+++ test/jise/type_test.clj
@@ -2,9 +2,8 @@
   (:require [clojure.test :refer [deftest testing is are]]
             [jise.type :as t])
   (:import [clojure.asm Type]
-           [java.lang.reflect Modifier]))
-
-(import 'java.io.BufferedReader)
+           [java.lang.reflect Modifier]
+           [java.io BufferedReader]))
 
 (deftest tag->type-test
   (testing "symbol representing primitive type can be converted to corresponding Type"
@@ -103,7 +102,8 @@
       t/OBJECT t/STRING
       (t/tag->type 'java.io.Closeable) (t/tag->type 'java.io.Reader)
       (t/tag->type 'Cloneable) (t/tag->type '[int])
-      (t/tag->type '[Object]) (t/tag->type '[String]))
+      (t/tag->type '[Object]) (t/tag->type '[String])
+      t/STRING nil)
     (let [cenv {:classes {'foo.bar.C
                           {:parent t/OBJECT
                            :interfaces #{(t/tag->type 'Runnable)}}}}]
@@ -114,7 +114,8 @@
     (are [t1 t2] (not (t/super? {} t1 t2))
       t/BOOLEAN t/INT
       t/INT t/BOOLEAN
-      (t/tag->type '[long]) (t/tag->type '[int]))))
+      (t/tag->type '[long]) (t/tag->type '[int])
+      nil t/STRING)))
 
 (deftest modifiers->access-flags-test
   (are [ms expected]
@@ -166,3 +167,85 @@
       t/FLOAT t/DOUBLE
       t/CHAR t/STRING
       t/STRING t/CHAR)))
+
+(deftest boxing-conversion-test
+  (testing "t1 can be boxed to t2"
+    (are [t1 t2]
+        (= {:conversion :boxing :from t1 :to t2}
+           (t/boxing-conversion t1))
+      t/BOOLEAN t/BOOLEAN_CLASS
+      t/BYTE t/BYTE_CLASS
+      t/CHAR t/CHARACTER_CLASS
+      t/SHORT t/SHORT_CLASS
+      t/INT t/INTEGER_CLASS
+      t/LONG t/LONG_CLASS
+      t/FLOAT t/FLOAT_CLASS
+      t/DOUBLE t/DOUBLE_CLASS))
+  (testing "t cannot be boxed"
+    (are [t] (= nil (t/boxing-conversion t))
+      t/BOOLEAN_CLASS
+      t/STRING
+      (t/tag->type '[int]))))
+
+(deftest unboxing-conversion-test
+  (testing "t1 can be unboxed to t2"
+    (are [t1 t2]
+        (= {:conversion :unboxing :from t1 :to t2}
+           (t/unboxing-conversion t1))
+      t/BOOLEAN_CLASS t/BOOLEAN
+      t/BYTE_CLASS t/BYTE
+      t/CHARACTER_CLASS t/CHAR
+      t/SHORT_CLASS t/SHORT
+      t/INTEGER_CLASS t/INT
+      t/LONG_CLASS t/LONG
+      t/FLOAT_CLASS t/FLOAT
+      t/DOUBLE_CLASS t/DOUBLE))
+  (testing "t cannot be unboxed"
+    (are [t] (= nil (t/unboxing-conversion t))
+      t/BOOLEAN
+      t/STRING
+      (t/tag->type '[Integer]))))
+
+(deftest widening-reference-conversion-test
+  (testing "t1 can be widened to t2"
+    (are [t1 t2]
+        (= {:conversion :widening-reference :from t1 :to t2}
+           (binding [*ns* (the-ns 'jise.type-test)]
+             (t/widening-reference-conversion {} t1 t2)))
+      t/STRING t/OBJECT
+      (t/tag->type 'java.io.BufferedReader) (t/tag->type 'java.io.Closeable)
+      (t/tag->type '[String]) (t/tag->type '[Object])
+      nil t/STRING)
+    (let [r (t/tag->type 'java.io.Reader)
+          cenv {:classes {'foo.bar.C {:parent r}}}
+          c (t/tag->type cenv 'foo.bar.C)]
+      (is (= {:conversion :widening-reference :from c :to r}
+             (t/widening-reference-conversion cenv c r)))))
+  (testing "t1 cannot be widened to t2"
+    (are [t1 t2] (= nil (t/widening-reference-conversion {} t1 t2))
+      t/INT t/LONG
+      t/OBJECT t/STRING
+      t/STRING (t/tag->type 'java.io.Closeable)
+      t/STRING nil)))
+
+(deftest narrowing-reference-conversion-test
+  (testing "t1 can be narrowed to t2"
+    (are [t1 t2]
+        (= {:conversion :narrowing-reference :from t1 :to t2}
+           (t/narrowing-reference-conversion {} t1 t2))
+      t/OBJECT t/STRING
+      (t/tag->type 'java.io.Closeable) (t/tag->type 'java.io.BufferedReader)
+      (t/tag->type '[Object]) (t/tag->type '[String])
+      t/STRING nil)
+    (let [r (t/tag->type 'java.io.Reader)
+          cenv {:classes {'foo.bar.C {:parent r}}}
+          c (t/tag->type cenv 'foo.bar.C)]
+      (is (= {:conversion :narrowing-reference :from r :to c}
+             (t/narrowing-reference-conversion cenv r c)))))
+  (testing "t1 cannot be narrowed to t2"
+    (are [t1 t2]
+        (= nil (t/narrowing-reference-conversion {} t1 t2))
+      t/LONG t/INT
+      t/STRING t/OBJECT
+      ;(t/tag->type 'java.io.Closeable) t/STRING
+      nil t/STRING)))
